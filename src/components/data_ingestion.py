0fe5d2c1571c9250e5ea13dbf8ac68c6397eed16
Harish Ahamed <36252984+harishahamed26@users.noreply.github.com>
2024-12-03 06:51:04 +0000

    hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -65,5 +65,5 @@ if __name__ == '__main__':
 
 
     model_trainer = ModelTrainer()
-    print(model_trainer.initiate_model_trainer(train_arr, test_arr,))
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
     
--- src/util.py
+++ src/util.py
@@ -11,6 +11,7 @@ from src.logger import logging
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_objects(file_path, obj):
     try:
@@ -23,14 +24,20 @@ def save_objects(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
-            model.fit(X_train, y_train)
+            gs = GridSearchCV(model, para, cv = 3)
+
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train) 
 
             y_train_pred = model.predict(X_train)
 
